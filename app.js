8e58da014b0897aae8e61cf744ee330309b41404
sudoburger <59908210+sudoburger@users.noreply.github.com>
2020-05-16 16:49:25 +1000

    add variable speeds

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const btn = document.querySelector('.talk');
 const content = document.querySelector('#contentArea');
 const reply = document.querySelector('#replyArea');
-
+var volume = 1;
+var rate = 1;
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -42,6 +43,10 @@ function readOutLoud(message) {
 	const speech = new SpeechSynthesisUtterance();
 	const finalText = unspecified[Math.floor(Math.random() * unspecified.length)];
 	speech.text = finalText;
+	speech.volume = volume;
+	
+	speech.pitch = 1;
+	speech.volume = volume;
 	if(message.includes('how')){
 		const finalText = greetings[Math.floor(Math.random() * greetings.length)];
 		
@@ -79,17 +84,31 @@ function readOutLoud(message) {
 		speech.text = dateTime + ". If I can work that out then why the fuck can't you?";
 		reply.textContent = dateTime + ". If I can work that out then why the fuck can't you?";
 	}
+	else if(message.includes('calm')){
+		rate--;
+		speech.rate = rate;
+		speech.text = "Okay, grandma, I'm calm.";
+		reply.textContent = "Okay grandma. I'm calm."
+	}
+
+	else if(message.includes('hurry up')){
+		rate++;
+		rate++;
+		speech.rate=rate;
+		speech.text = "Gotcha kiddo. This good?";
+		reply.textContent = "Gotcha kiddo. This good?";
+	}
 	else{
 		reply.textContent = finalText;
+		speech.rate=rate;
+		
 	}
 	
 
 	
-	speech.volume = 1;
-	speech.rate = 1;
-	speech.pitch = 1;
-	
 
+	
+	
 	window.speechSynthesis.speak(speech);
 	console.log("Should be speaking now. Speaking function completed.");
 
